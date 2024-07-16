5fb506dcb48e3b899d99748e0ceac262dba2b4eb
ram0973 <ram0973@gmail.com>
2024-07-16 16:10:48 +0300

    initial commit

---
+++ src/main/java/com/ram0973/booknetwork/user/User.java
@@ -0,0 +1,39 @@
+package com.ram0973.booknetwork.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Principal;
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+
+public class User implements UserDetails, Principal {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+}
